becf3fd6dc8fa2770ff357b7e0bc67774b817877
Andrew Sinner <andrewsinner@gmail.com>
2017-08-09 16:18:21 -0700

    Add Connection interface (#22)

---
+++ src/data/ConferenceConnection.ts
@@ -0,0 +1,13 @@
+import { IConfOutgoingMessage, IConfIncomingMessage } from './Conference';
+
+// NOTE(andrews): ConferenceConnection is an interface that your connection object
+// must satisfy. The Conference component will depend on this interface when it calls
+// the createConnection API.
+export interface ConferenceConnection {
+    subscribe: (subscriber: ConferenceConnectionSubscriber) => void;
+    publish: (message: IConfOutgoingMessage) => void;
+}
+
+export interface ConferenceConnectionSubscriber {
+    (message: IConfIncomingMessage): void;
+}
