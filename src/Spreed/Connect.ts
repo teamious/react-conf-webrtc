f43706c2921fd93c5675fff3c2aec443c6b9fcd7
Andrew Sinner <andrewsinner@gmail.com>
2017-08-25 16:00:01 -0700

    Add support in Connection class for subscribing to event stream (#88)
    * Add support in Connection class for subscribing to event stream
    
    * Modify names

--- src/Spreed/Connect.ts
+++ src/Spreed/Connect.ts
@@ -2,25 +2,45 @@ import { ConferenceConnection, ConferenceConnectionSubscriber } from '../data/Co
 import { IConfIncomingMessage, IConfOutgoingMessage } from '../data/ConferenceMessage';
 import { SpreedConnection } from './SpreedConnection';
 import { SpreedAdapter } from './SpreedAdapter';
+import { SpreedResponse } from '../Spreed';
 
-export function Connect(url: string): ConferenceConnection {
+export function Connect(url: string): Connection {
     return new Connection(url);
 }
 
-// NOTE(andrews): Connection
-class Connection implements ConferenceConnection {
+// NOTE(andrews): Connection class is responsible for controlling the communication
+// between the SpreedConnection and SpreedAdapter instances. It exposes an API
+// for subscribers to hook into the SpreedConnection event stream. This means
+// that anytime the SpreedServer sends a message to the SpreedConnection, you
+// can be notified.
+export class Connection implements ConferenceConnection {
     private conn: SpreedConnection;
     private adapter: SpreedAdapter;
+    private onConnMessageHandler?: SpreedConnectionMessageHandler;
 
     constructor(url: string) {
         this.conn = new SpreedConnection(url);
         this.adapter = new SpreedAdapter();
         this.conn.onmessage = (msg) => {
+            if (this.onConnMessageHandler) {
+                this.onConnMessageHandler(msg, () => {
+                    this.adapter.handleSpreedMessage(msg);
+                })
+                return
+            }
             this.adapter.handleSpreedMessage(msg);
         }
         this.adapter.onSpreedMessage = (msg) => {
             this.conn.send(msg);
         }
+    }
+
+    // NOTE(andrews): Use this API if you want to subscribe to the incoming event
+    // stream from the SpreedConnection class. Note that done() must be called
+    // by your handler if you want the message to continue through to the
+    // connection.
+    set onconnmessage(handler: SpreedConnectionMessageHandler) {
+        this.onConnMessageHandler = handler;
     }
 
     public subscribe(handler: ConferenceConnectionSubscriber) {
@@ -32,4 +52,8 @@ class Connection implements ConferenceConnection {
     public publish(msg: IConfOutgoingMessage) {
         this.adapter.handleConferenceMessage(msg);
     }
+}
+
+export interface SpreedConnectionMessageHandler {
+    (message: SpreedResponse, done: () => void): void
 }
